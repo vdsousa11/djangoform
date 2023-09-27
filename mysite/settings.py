7efe3175a6c661ec0adaa7c289bf97d14ecf1ffe
Frank <chicocortereal@gmail.com>
2023-09-27 10:42:52 +0100

    added streamlit import

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,4 +128,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'chicocortereal@gmail.com'
-EMAIL_HOST_PASSWORD = 'pjbcmzuormwqvfjx'
+EMAIL_HOST_PASSWORD = ''
